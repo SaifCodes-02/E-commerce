f13d1420d9b7e5ce0cd22b7780ebe3807d96cf96
SaifCodes-02 <saifijamil00@gmail.com>
2024-12-27 23:40:05 +0500

    Changed file name

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../Components/Hero';
 import Specs from '../Components/Specs';
 import Arrivals from '../Components/Arrivals';
 import HighlightsSlider from '../Components/HighlightsSlider';
-import ShopNow from '../Components/ShopNow'; // Import ShopNow component
+import ShopNow from '../Components/Shopnow'; // Import ShopNow component
 import Footer from '../Components/Footer';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -20,7 +20,9 @@ const Home = () => {
       <div>
         <Arrivals />
       </div>
+      
       <HighlightsSlider />
+
       <div className='mb-3'>  <ShopNow /></div>
 <Footer/>
      {/* Use the ShopNow component */}
