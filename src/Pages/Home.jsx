e8c9474cf6c4300fa334fedd9db5caa05493748c
SaifCodes-02 <saifijamil00@gmail.com>
2025-02-05 22:54:02 +0500

    Web banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from '../Components/Hero';
 import Specs from '../Components/Specs';
 import Arrivals from '../Components/Arrivals';
 import HighlightsSlider from '../Components/HighlightsSlider';
-
 import Footer from '../Components/Footer';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
