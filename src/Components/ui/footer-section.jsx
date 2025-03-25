1880238476360ab2544c5f576e7d1b86fc1dbcde
SaifCodes-02 <saifijamil00@gmail.com>
2025-03-26 01:11:37 +0500

    added new footer + checkout page

--- src/Components/ui/footer-section.jsx
+++ src/Components/ui/footer-section.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button } from "@/components/ui/button";
+import { button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
